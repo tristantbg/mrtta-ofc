656d47e7caf766036bbf3e3008b961dcf018eec6
Tristan Bagot <tristan.bagot@gmail.com>
2018-05-27 21:40:28 +0200

    test

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 // import d'un autre composant
-import Flickity from 'flickity';
 import main from './main.styl';
+import Flickity from 'flickity';
 
-main();
+const App = {
+  init: () => {
+    // const iframes = document.querySelectorAll('iframe')
+
+    // for (var i = 0; i < iframes.length; i++) {
+    //   iframes[i].src = iframes[i].dataset.src
+    // }
+    App.current = 0
+    App.books = document.querySelectorAll('.book')
+
+    // var flkty = new Flickity( books, {
+    //   'cellSelector': '.book',
+    // });
+
+    document.getElementById('prev').addEventListener('click', App.prev)
+    document.getElementById('next').addEventListener('click', App.next)
+    document.getElementById('corner').addEventListener('click', App.intro)
+  },
+  intro: () => {
+    document.body.classList.toggle('intro')
+  },
+  next: () => {
+    App.books[App.current].style.visibility = 'hidden'
+    App.current = (App.current + 1)%(App.books.length - 1)
+    console.log(App.current)
+    App.books[App.current].style.visibility = 'visible'
+  },
+  prev: () => {
+    App.books[App.current].style.visibility = 'hidden'
+    App.current = App.current - 1
+    if (App.current < 0) App.current = App.books.length - 1
+    console.log(App.current)
+    App.books[App.current].style.visibility = 'visible'
+  }
+}
+
+document.addEventListener('DOMContentLoaded', App.init)
--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 // import d'un module CSS
 import classes from './main.scss';
+import Flickity from 'flickity';
 
 export default () => {
   console.log(classes.main);
 };
+
+const App = {
+  init: () => {
+    const iframes = document.querySelectorAll('iframe')
+
+    for (var i = 0; i < iframes.length; i++) {
+      iframes[i].src = iframes[i].dataset.src
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', App.init)
